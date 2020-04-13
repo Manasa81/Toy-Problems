51237a2048350b90dba8053d8e2b227de8d0ff2b
Manasa81 <kallurimanasa10@msitprogram.net>
2020-04-13 22:28:15 +0530

    Methods added

--- LRUcache.py
+++ LRUcache.py
@@ -5,5 +5,12 @@ class LRUcache:
     def __init__(self, w, u,d): 
         self.websiteName = w 
         self.url=u
-        self.date=d 
+        self.date=d
+    def put(self,w,u,d):
+        pass
+    def get(self,w):
+        pass
+    def get_cache(self):
+        pass
+
       
