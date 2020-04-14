6f06795f0a0eea655e2f908e8fb93eef7ad089b9
Manasa81 <kallurimanasa10@msitprogram.net>
2020-04-14 19:46:35 +0530

    Testing Completed

--- LRU.py
+++ LRU.py
@@ -1,14 +1,32 @@
+from queue import Queue 
+  
+# Initializing a queue 
 class LRU:
-    websiteName = ''
     url = ''
     date=''
-    def __init__(self, w, u,d): 
-        self.websiteName = w 
+    l=[]
+    q = Queue(maxsize = 3) 
+    def __init__(self, u,d): 
         self.url=u
         self.date=d
-    def put(self,a):
+    def put(self,url):
+        if len(self.l) == 3:
+            self.l.pop(0)
+        self.l.append(url)
         return 'done'
     def get(self):
-        pass
+        return len(self.l) == 3
     def get_cache(self):
-        pass
+        return self.l
+# o = LRU('a',1)
+# o.put('a')
+# o.put('b')
+# o.put('c')
+# o.put('d')
+
+# print(o.get())
+# print(o.get_cache())
+
+
+
+
--- test_LRU.py
+++ test_LRU.py
@@ -1,16 +1,27 @@
 import unittest
-import LRU
+from LRU import LRU
 
 class TestLRU(unittest.TestCase):
 
 
     def test_put(self):
-        obj = LRU.LRU("web","a",10)
-        self.assertEqual(obj.put(10),'done')
+        obj = LRU('a',1)
+        self.assertEqual(obj.put('a'),'done')
     def test_get(self):
-        pass
-    def test_get_cache:
-        pass
+        obj = LRU('a',1)
+        obj.put('a')
+        obj.put('b')
+        # obj.put('c')
+        # print(obj.l)
+        self.assertEqual(obj.get(),False)
+
+    def test_get_cache(self):
+        obj = LRU('a',1)
+        obj.put('a')
+        obj.put('b')
+        obj.put('c')
+        obj.put('d')
+        self.assertEqual(obj.get_cache(),['b','c','d'])
 if __name__ == '__main__':
     unittest.main()
 
